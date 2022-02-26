c3470302afdbf0a9e6b8e011d7f60946b7543658
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-02-26 05:46:22 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mitch Andrade on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
